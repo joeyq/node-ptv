aa03d1ed1f10a9c62d3f228b043d3c7b531753ff
Joey Lee <jayd005@gmail.com>
2015-06-19 17:18:07 +1000

    added new functions for new API

--- index.js
+++ index.js
@@ -14,146 +14,182 @@ var request = require('request');
 
 var endpoint = "http://timetableapi.ptv.vic.gov.au";
 
-
 function createSignature(key, url, args) {
-  return crypto.createHmac('sha1', key)
-    .update(urlFormat({pathname: '/v2' + url, query: args}))
-    .digest('hex').toUpperCase();
+    return crypto.createHmac('sha1', key)
+        .update(urlFormat({pathname: '/v2' + url, query: args}))
+        .digest('hex').toUpperCase();
 }
 
 function PTV(opts) {
-  this.devId = opts.devId;
-  this.key = opts.key;
-  this._activeReqs = 0;
+    this.devId = opts.devId;
+    this.key = opts.key;
+    this._activeReqs = 0;
 }
 
-PTV.prototype._callAPIutc = function(url, utc, cb) {
-  var result;
-  var query = { devid: this.devId };
-  if (utc)
-    query.for_utc = utc.toISOString();
-  var signature = createSignature(this.key, url, query);
-  query.signature = signature;
-  this._activeReqs++;
-  var ptv = this;
-  request({
-    url: endpoint + '/v2' + url,
-    qs: query
-  }, function(error, response, body) {
-    ptv._activeReqs--;
-    if (!error && response.statusCode == 200) {
-      try {
-        result = JSON.parse(body);
-      } catch(e) {
-        return cb(e);
-      }
-      cb(null, result);
-    } else {
-      if (error)
-        return cb(error);
-      cb(response.statusCode);
-    }
-  });
+PTV.prototype._callAPIutc = function (url, utc, cb) {
+    var result;
+    var query = {devid: this.devId};
+    if (utc)
+        query.for_utc = utc.toISOString();
+    var signature = createSignature(this.key, url, query);
+    query.signature = signature;
+    this._activeReqs++;
+    var ptv = this;
+    request({
+        url: endpoint + '/v2' + url,
+        qs: query
+    }, function (error, response, body) {
+        ptv._activeReqs--;
+        if (!error && response.statusCode == 200) {
+            try {
+                result = JSON.parse(body);
+            } catch (e) {
+                return cb(e);
+            }
+            cb(null, result);
+        } else {
+            if (error)
+                return cb(error);
+            cb(response.statusCode);
+        }
+    });
 };
 
-PTV.prototype._callAPI = function(url, cb) {
-  this._callAPIutc(url, null, cb);
+PTV.prototype._callAPI = function (url, cb) {
+    this._callAPIutc(url, null, cb);
 };
 
-
-PTV.prototype.stopsNearby = function(latitude, longitude, cb) {
-  this._callAPI(
-    util.format('/nearme/latitude/%d/longitude/%d', latitude, longitude),
-    function(err, res) {
-      if (err) return cb(err);
-      return cb(null, res.map(function(s) { return s.result; }));
-    }
-  );
+PTV.prototype.healthCheck = function () {
+    this._callAPI(
+        '/healthcheck',
+        function (err, res) {
+            console.log(res);
+        }
+    );
 };
 
-PTV.prototype.transportPOIsByMap = function(poi, lat1, long1, lat2, long2,
-  griddepth, limit, cb) {
-  this._callAPI(
-    util.format('/poi/%s/lat1/%d/long1/%d/lat2/%d/long2/%d/griddepth/%d/limit/%d',
-      encodeURI(poi), lat1, long1, lat2, long2, griddepth, limit),
-    cb
-  );
+PTV.prototype.stopsNearby = function (latitude, longitude, cb) {
+    this._callAPI(
+        util.format('/nearme/latitude/%d/longitude/%d', latitude, longitude),
+        function (err, res) {
+            if (err) return cb(err);
+            return cb(null, res.map(function (s) {
+                return s.result;
+            }));
+        }
+    );
 };
 
-PTV.prototype.search = function(what, cb) {
-  this._callAPI(
-    '/search/' + encodeURI(what),
-    cb
-  );
+PTV.prototype.transportPOIsByMap = function (poi, lat1, long1, lat2, long2,
+                                             griddepth, limit, cb) {
+    this._callAPI(
+        util.format('/poi/%s/lat1/%d/long1/%d/lat2/%d/long2/%d/griddepth/%d/limit/%d',
+            encodeURI(poi), lat1, long1, lat2, long2, griddepth, limit),
+        cb
+    );
+};
+
+PTV.prototype.search = function (what, cb) {
+    this._callAPI(
+        '/search/' + encodeURI(what),
+        cb
+    );
 };
 
 PTV.mode = {
-  train: 0,
-  tram: 1,
-  bus: 2,
-  vline: 3,
-  nightrider: 4
+    train: 0,
+    tram: 1,
+    bus: 2,
+    vline: 3,
+    nightrider: 4
 };
 
 PTV.modeName = ['train', 'tram', 'bus', 'vline', 'nightrider'];
 
-PTV.prototype.broadNextDepartures = function(mode, stop, limit, date, cb) {
-  if (!date)
-    date = new Date();
-  this._callAPIutc(
-    util.format('/mode/%d/stop/%d/departures/by-destination/limit/%d',
-      mode, stop, limit),
-    date,
-    function(err, res) {
-      if (err) return cb(err);
-      //if (!res || !res.values)
-      //  console.log(res);
-      cb(null, res.values);
-    }
-  );
+PTV.prototype.linesByMode = function (mode, name, cb) {
+    this._callAPI(
+        util.format('/lines/mode/%d?name=%s', mode, name),
+        cb
+    );
 };
 
-PTV.prototype.specificNextDepartures = function(mode, line, stop,
-  directionid, limit, date, cb) {
-  if (!date)
-    date = new Date();
-  this._callAPIutc(
-    util.format('/mode/%d/line/%d/stop/%d/directionid/%d/departures/all/limit/%d',
-      mode, line, stop, directionid, limit),
-    date,
-    function(err, res) {
-      if (err) return cb(err);
-      //if (!res || !res.values)
-      //  console.log(res);
-      cb(null, res.values);
-    }
-  );
+PTV.prototype.broadNextDepartures = function (mode, stop, limit, date, cb) {
+    var modeID = PTV.modeName.indexOf(mode);
+    if (!date)
+        date = new Date();
+    this._callAPIutc(
+        util.format('/mode/%d/stop/%d/departures/by-destination/limit/%d',
+            modeID, stop, limit),
+        date,
+        function (err, res) {
+            if (err) return cb(err);
+            //if (!res || !res.values)
+            //  console.log(res);
+            cb(null, res.values);
+        }
+    );
 };
 
-PTV.prototype.stoppingPattern = function(mode, run, stop, date, cb) {
-  if (!date)
-    date = new Date();
-  this._callAPIutc(
-    util.format('/mode/%d/run/%d/stop/%d/stopping-pattern',
-      mode, run, stop),
-    date,
-    function(err, res) {
-      if (err) return cb(err);
-      cb(null, res.values);
-    }
-  );
+PTV.prototype.specificNextDepartures = function (mode, line, stop,
+                                                 directionid, limit, date, cb) {
+    if (!date)
+        date = new Date();
+    this._callAPIutc(
+        util.format('/mode/%d/line/%d/stop/%d/directionid/%d/departures/all/limit/%d',
+            mode, line, stop, directionid, limit),
+        date,
+        function (err, res) {
+            if (err) return cb(err);
+            //if (!res || !res.values)
+            //  console.log(res);
+            cb(null, res.values);
+        }
+    );
 };
 
-PTV.prototype.stopsOnALine = function(mode, line, cb) {
-  this._callAPI(
-    util.format('/mode/%d/line/%d/stops-for-line', mode, line),
-    cb
-  );
+PTV.prototype.stoppingPattern = function (mode, run, stop, date, cb) {
+    var modeID = PTV.modeName.indexOf(mode);
+    if (!date)
+        date = new Date();
+    this._callAPIutc(
+        util.format('/mode/%d/run/%d/stop/%d/stopping-pattern',
+            modeID, run, stop),
+        date,
+        function (err, res) {
+            if (err) return cb(err);
+            cb(null, res.values);
+        }
+    );
 };
+
+PTV.prototype.stopsOnALine = function (mode, line, cb) {
+    this._callAPI(
+        util.format('/mode/%d/line/%d/stops-for-line', mode, line),
+        cb
+    );
+};
+
+PTV.disruptionModes = ['general', 'metro-bus', 'metro-train', 'metro-tram',
+    'regional-bus', 'regional-coach', 'regional-train'];
+
+PTV.prototype.disruptions = function (mode, cb) {
+    var modes = "";
+    for (var i = 0; i < mode.length; i++) {
+        modes += mode[i];
+        if (i != mode.length - 1) {
+            modes += ",";
+        }
+    }
+    this._callAPI(
+        util.format('/disruptions/modes/%s', modes),
+        cb
+    );
+};
+
 
 module.exports = PTV;
-module.exports.createClient = function(opts) {
-  return new PTV(opts);
+module.exports.createClient = function (opts) {
+    return new PTV(opts);
 };
 
 //var pt = new PTV({ key: key, devId: devId});
